48937ee6345ea8c0d6e17d047d8271964a004ccd
Törőcsik Ádám <toarexer@gmail.com>
2025-02-26 21:47:10 +0100

    Added certificates and generator scripts

---
+++ gen-rca.ps1
@@ -0,0 +1,40 @@
+#!/usr/bin/env pwsh
+
+if (!(Get-Command 'openssl' -ErrorAction SilentlyContinue)) {
+	Write-Host 'Error: openssl is not installed!' -ForegroundColor Red
+	exit 1
+}
+
+$originalPath = Get-Location
+$certsDir = "$PSScriptRoot/certs"
+
+$caKey = 'rca.key'
+$caCer = 'rca.crt'
+
+$domain = 'boilerplate.hu'
+$subject = "/C=HU/ST=Pest/L=Budapest/O=Boilerplate Certificate Authority/CN=$domain"
+
+$keySize = 2048
+$days = 365
+
+try {
+	New-Item $certsDir -Force -ItemType Directory | Out-Null
+	Set-Location $certsDir
+
+	Write-Host "Generating $caKey"
+	openssl genrsa -out $caKey $keySize
+
+	Write-Host "Creating $caCer"
+	openssl req `
+		-days $days `
+		-key $caKey `
+		-new `
+		-out $caCer `
+		-subj $subject `
+		-x509 
+
+	Write-Host 'Done!'
+}
+finally {
+	Set-Location $originalPath
+}
--- put-conf.ps1
+++ put-conf.ps1
@@ -1,16 +1,16 @@
 #!/usr/bin/env pwsh
 
-if (!(Get-Command "sftp" -ErrorAction SilentlyContinue)) {
-	Write-Host "Error: sftp is not installed!" -ForegroundColor Red
+if (!(Get-Command 'sftp' -ErrorAction SilentlyContinue)) {
+	Write-Host 'Error: sftp is not installed!' -ForegroundColor Red
 	exit 1
 }
 
 $originalPath = Get-Location
 Set-Location $PSScriptRoot
 
-@"
+@'
 put router/*.rsc
 bye
-"@ | sftp 'admin@192.168.1.254'
+'@ | sftp 'admin@192.168.1.254'
 
 Set-Location $originalPath
