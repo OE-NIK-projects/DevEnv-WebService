025add2eb1aac7aea011f9e8b996b0441ab9ba4b
Törőcsik Ádám <toarexer@gmail.com>
2025-02-26 17:26:23 +0100

    Updated putconf script

--- putconf.ps1
+++ putconf.ps1
@@ -1,8 +1,16 @@
 #!/usr/bin/env pwsh
 
-$cmds = @"
+if (-not (Get-Command "sftp" -ErrorAction SilentlyContinue)) {
+	Write-Host "Error: sftp is not installed!" -ForegroundColor Red
+	exit 1
+}
+
+$originalPath = Get-Location
+Set-Location $PSScriptRoot
+
+@"
 put router/*.rsc
 bye
-"@
+"@ | sftp 'admin@192.168.1.254'
 
-$cmds | sftp 'admin@192.168.1.254'
+Set-Location $originalPath
