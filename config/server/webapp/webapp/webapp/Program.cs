616978d186b98cb7b5ce01e157399b00734a41ca
Szidor Patrik <patrikp987@gmail.com>
2025-03-08 14:50:32 +0100

    Created Blazor webapp with MudBlazor template

---
+++ config/server/webapp/webapp/webapp/Program.cs
@@ -0,0 +1,35 @@
+using MudBlazor.Services;
+using webapp.Client.Pages;
+using webapp.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add MudBlazor services
+builder.Services.AddMudServices();
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(webapp.Client._Imports).Assembly);
+
+app.Run();
