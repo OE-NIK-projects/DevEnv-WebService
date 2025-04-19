f165477f4e5f8a8e4e2c1dd9b19b6a1ef4e70158
Szidor Patrik <patrikp987@gmail.com>
2025-04-19 16:52:17 +0200

    Created Users object

---
+++ config/server/scripts/values.ps1
@@ -0,0 +1,10 @@
+$Users = @(
+    @{ Username = "admin"; Email = "admin@boilerplate.lan"; Password = "admin"; Admin = $true }
+    @{ Username = "teszt.elek"; Email = "teszt.elek@boilerplate.lan"; Password = "Password1!"; Admin = $false }
+    @{ Username = "mezo.gyorgy"; Email = "mezo.gyorgy@boilerplate.lan"; Password = "Password1!"; Admin = $false }
+    @{ Username = "toth.laci"; Email = "toth.laci@boilerplate.lan"; Password = "Password1!"; Admin = $false }
+    @{ Username = "gabi.roli"; Email = "gabi.roli@boilerplate.lan"; Password = "Password1!"; Admin = $false }
+    @{ Username = "xanax.enjoyer"; Email = "xanax.enjoyer@boilerplate.lan"; Password = "Password1!"; Admin = $false }
+    @{ Username = "jungle.diff"; Email = "jungle.diff@boilerplate.lan"; Password = "Password1!"; Admin = $false }
+)
+
