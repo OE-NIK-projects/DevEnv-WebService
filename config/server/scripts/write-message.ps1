33f1f747d036ce052d48dab46ea3a6621aadb238
Szidor Patrik <patrikp987@gmail.com>
2025-04-19 16:53:34 +0200

    Created function for pretty logging

---
+++ config/server/scripts/write-message.ps1
@@ -0,0 +1,27 @@
+function Write-Message {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$Message,
+
+        [Parameter()]
+        [ValidateSet('Info', 'Error', 'Command', 'Success', 'Default')]
+        [string]$Type = 'Default'
+    )
+
+    $prefix = "[$Type]"
+    $color = switch ($Type) {
+        'Info' { 'Cyan' }
+        'Error' { 'Red' }
+        'Success' { 'Green' }
+        'Command' { 'Magenta' }
+        'Default' { 'White' }
+    }
+
+    if ($Type -eq 'Default') {
+        Write-Host "$Message" -ForegroundColor White
+    }
+    else {
+        Write-Host "$prefix $Message" -ForegroundColor $color
+    }
+}
