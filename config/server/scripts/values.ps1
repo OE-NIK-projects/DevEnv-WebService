a92f525beab55b3e62adbbeb0584c14b7e2327b5
Szidor Patrik <patrikp987@gmail.com>
2025-04-19 18:54:38 +0200

    Added Backend and Frontend groups

--- config/server/scripts/values.ps1
+++ config/server/scripts/values.ps1
@@ -1,23 +1,61 @@
+$TimeoutInSeconds = 2
+
 $Users = @(
-    @{ Username = "admin"; Email = "admin@boilerplate.lan"; Password = "admin"; Admin = $true }
-    @{ Username = "teszt.elek"; Email = "teszt.elek@boilerplate.lan"; Password = "Password1!"; Admin = $false }
-    @{ Username = "mezo.gyorgy"; Email = "mezo.gyorgy@boilerplate.lan"; Password = "Password1!"; Admin = $false }
-    @{ Username = "toth.laci"; Email = "toth.laci@boilerplate.lan"; Password = "Password1!"; Admin = $false }
-    @{ Username = "gabi.roli"; Email = "gabi.roli@boilerplate.lan"; Password = "Password1!"; Admin = $false }
-    @{ Username = "xanax.enjoyer"; Email = "xanax.enjoyer@boilerplate.lan"; Password = "Password1!"; Admin = $false }
-    @{ Username = "jungle.diff"; Email = "jungle.diff@boilerplate.lan"; Password = "Password1!"; Admin = $false }
+    @{ 
+        Username = "admin"; 
+        Email    = "admin@boilerplate.lan"; 
+        Password = "admin"; 
+        Admin    = $true; 
+    },
+    @{ 
+        Username = "teszt.elek"; 
+        Email    = "teszt.elek@boilerplate.lan"; 
+        Password = "Password1!"; 
+        Admin    = $false; 
+    },
+    @{ 
+        Username = "mezo.gyorgy"; 
+        Email    = "mezo.gyorgy@boilerplate.lan"; 
+        Password = "Password1!"; 
+        Admin    = $false; 
+    },
+    @{ 
+        Username = "toth.laci"; 
+        Email    = "toth.laci@boilerplate.lan"; 
+        Password = "Password1!"; 
+        Admin    = $false; 
+    },
+    @{ 
+        Username = "gabi.roli"; 
+        Email    = "gabi.roli@boilerplate.lan"; 
+        Password = "Password1!"; 
+        Admin    = $false; 
+    },
+    @{
+        Username = "xanax.enjoyer"; 
+        Email    = "xanax.enjoyer@boilerplate.lan"; 
+        Password = "Password1!"; 
+        Admin    = $false; 
+    },
+    @{ 
+        Username = "jungle.diff"; 
+        Email    = "jungle.diff@boilerplate.lan"; 
+        Password = "Password1!"; 
+        Admin    = $false; 
+    }
 )
 
-$Organization = @{
-    Username                      = "Boilerplate";
-    Description                   = "Innovatív, Clean Code elveit betartó Java gyűlölő cég.";
-    Full_name                     = "Boilerplate Kft.";
-    Email                         = "boilerplate@boilerplate.lan";
-    Location                      = "Budapest";
-    Repo_admin_change_team_access = $true;
-    Visibility                    = "public";
-    #TODO: Create DNS, Nginx with SSL
-    #Website                       = "https://server.lan"
-}
+#Groups
+$Organizations = @(
+    @{
+        Username                      = "Frontend";
+        Description                   = "Frontendesek";
+        Repo_admin_change_team_access = $true;
+    }
+    @{
+        Username                      = "Backend";
+        Description                   = "Backendesek";
+        Repo_admin_change_team_access = $true;
+    }
+)
 
-$TimeoutInSeconds = 5
