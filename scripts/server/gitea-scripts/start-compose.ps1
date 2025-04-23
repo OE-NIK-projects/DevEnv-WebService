2463a5c60957b5143a1f194d3ad658773d96ed63
Szidor Patrik <patrikp987@gmail.com>
2025-04-23 14:11:01 +0200

    Starting docker compose with sudo

--- scripts/server/gitea-scripts/start-compose.ps1
+++ scripts/server/gitea-scripts/start-compose.ps1
@@ -3,5 +3,5 @@
 Write-Message -Message "Starting $($PSScriptRoot)/../docker-compose.yml" -Type Info
 $originalDir = Get-Location
 Set-Location -Path "$PSScriptRoot/../"
-docker compose up -d
+sudo docker compose up -d
 Set-Location -Path $originalDir
